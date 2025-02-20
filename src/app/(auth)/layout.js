e203f9f4f72c78ce2dbab361625f44b48b6789b5
Pushan <mukhopadhyaypushan42@gmail.com>
2025-02-20 22:25:00 +0530

    adding inngest and neoDb and signup and signin (routing)

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AutherLayout({children}) {
+  return (
+    <div className='flex justify-center pt-20 pb-3'>{children}</div>
+  )
+}
+
+export default AutherLayout
